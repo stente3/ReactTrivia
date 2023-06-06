74d45993477d4c5d2253b84a95591b0ad081a0dc
Stente3 <stentem0103@gmail.com>
2023-06-06 16:37:50 -0500

    Feature(App.jsx): Complete main component styling with Tailwind CSS
    This commit represents the completion of the main component styling using the Tailwind CSS framework.
    Tailwind CSS was chosen as the styling solution for this project, and the main component has now been fully implemented according to the design and functional requirements.

---
+++ src/components/App/index.jsx
@@ -0,0 +1,13 @@
+import Header from '../Header';
+import Main from '../Main';
+
+function App() {
+  return (
+    <div className='bg-gray-800 min-h-screen'>
+      <Header />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
